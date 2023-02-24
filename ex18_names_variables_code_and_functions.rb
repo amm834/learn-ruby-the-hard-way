16f7ad4e2da1d79c87e697b852c6980e8ffd2ac4
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-02-24 22:59:38 +0630

    learn: functions and arg passing

---
+++ ex18_names_variables_code_and_functions.rb
@@ -0,0 +1,17 @@
+def get_args(*args)
+  first, second = args
+  puts "first: #{first}"
+  puts "first: #{second}"
+end
+
+def get_one(first)
+  puts "first #{first}"
+end
+
+def get_two(first, second)
+  puts "fist #{first}, second #{second}"
+end
+
+get_args("one", "two")
+get_one("hello")
+get_two("not", "again")
