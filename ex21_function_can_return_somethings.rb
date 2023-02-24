8d9a8cf730d402b60870c338f92f4c8196c67f75
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-02-24 23:10:38 +0630

    learn: function and return value

---
+++ ex21_function_can_return_somethings.rb
@@ -0,0 +1,22 @@
+def add(a, b)
+  return a + b
+end
+
+def minus(a, b)
+  return a - b
+end
+
+def multiply(a, b)
+  return a * b
+end
+
+def divide(a, b)
+  return a / b
+end
+
+def modulo(a, b)
+  return a % b
+end
+
+total = add(1, 3)
+puts "total: #{total}"
